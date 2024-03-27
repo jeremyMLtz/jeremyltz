59a3e30c115fa054c103764ab82ee17121c5f171
jeremyMLtz <jeremy.m.lentz@gmail.com>
2024-03-27 13:41:47 -0500

    Added new projects

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -5,31 +5,32 @@ const About = forwardRef<HTMLElement, {}>(({}, ref: Ref<HTMLElement>) => (
     <h3 className='section-header'>About</h3>
     <div className='about-content'>
       <p>
-        The year was 2016. I had always loved creating stories and building
-        worlds, and I decided it was time to try telling stories using a new
-        medium: building a video game. After watching countless YouTube
-        tutorials, reading documentation, and fixing bugs, I realized my passion
-        was for the programming aspect of game development. I knew that I wanted
-        to write code for a living, whether it was for games or the web.
+        My journey in software engineering began the same as alot of the other
+        engineers I know...with game development. It started off as a hobby but
+        after programming a few games, I found writing code was fun and I wanted
+        to try my hand at web development.
       </p>
       <p>
         Fast forward a few months, and I found myself interviewing for a sales
         job at a tech company. I was hired by <strong>BombBomb</strong> as a
         BDR, responsible for scheduling product demos for our sales team.
-        However, I quickly transitioned into a QA role, and that's when my
-        engineering skills really started to flourish. In between taking Udemy
-        or Treehouse courses, I immersed myself in reviewing pull requests from
-        our senior developers, writing automated tests, and even doing some full
-        stack work. It didn't take long before I was promoted to an engineering
-        role, and from that point on, there was no looking back.
+        However, I quickly transitioned into a QA Engineer role, where I helped
+        build the department from the ground up. I spent my time developing QA
+        processes, building out our automation suite from the ground up and
+        mentoring new QA Analysts. After a few years, I finally moved into my
+        goal role as a Software Engineer. Throughout the years as an engineer I
+        built an A/B onboarding tool that drove user adoption updwards of 50%,
+        lead the development of a new feature that generated over $1M in sales,
+        mentored junior engineers, was a lead member of our Module Library
+        Guild, and championed writing well-tested code.
       </p>
       <p>
-        These days, I dedicate most of my time to working on my startup
+        These days, in between looking for my next career opportunity, I
+        dedicate most of my time to working on my startup
         <strong> SimplMachine</strong>, where we develop solutions for
-        automation engineers. Simultaneously, I'm always on the lookout for new
-        opportunities. When I'm not coding, you can find me exploring the woods,
-        creating music, working on some creative writing, or developing my dream
-        game (stay tuned for more updates on that soon).
+        automation engineers while also developing my dream game. When I'm not
+        coding, you can find me exploring the woods, creating music, or taking
+        pictures of birds.
       </p>
     </div>
   </section>
--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -3,19 +3,12 @@ const Intro = () => (
     <span className='mono intro'>Howdy, my name is</span>
     <h1 className='intro-heading'>Jeremy Lentz</h1>
     <h2 className='intro-heading'>
-      Co-Founder and Lead Developer at SimplMachine
+      Software Engineer Building Things for the Web.
     </h2>
     <p className='intro-blurb'>
-      I'm a full stack engineer specialized in building clean and performant
-      user experiences for the internet.
+      I'm a front-end engineer specialized in building clean and performant user
+      experiences for the internet.
     </p>
-    <a
-      className='resume-link'
-      href='/resume.pdf'
-      target='_blank'
-      rel='noopener noreferrer'>
-      Resume
-    </a>
   </section>
 );
 
